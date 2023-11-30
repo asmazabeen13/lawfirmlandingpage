4e26995ab401a01466eba6e1c299fd13fe5391fe
asma zabeen <99760535+asmazabeen13@users.noreply.github.com>
2023-11-30 22:03:17 +0400

    Update HeroContent.jsx

--- Law_firm-main/src/Components/HeroContent/HeroContent.jsx
+++ Law_firm-main/src/Components/HeroContent/HeroContent.jsx
@@ -5,18 +5,18 @@ import hero from "../../assets/hero.svg"
 const HeroContent = () => {
     return (
         <div className='herocontent'>
-            <div className='leftcontent'>
+            <div className='content'>
                 <h1>You don’t have to <br /><span>Fight them Alone.</span></h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipiscing elit blandit,<br />
                     curabitur sodales conubia ut inceptos faucibus himenaeos tortor <br />
                     eget, hac massa gravida arcu interdum proin curae.</p>
-                <div className="input-container">
+                <div className="container">
                     <input type="text" placeholder="Enter your email address" />
                     <button type="button">Submit</button>
                 </div>
             </div>
             <div className='heroimg'>
-                <img src={hero} alt="kunalborkar2001@gmail.com" />
+                <img src={hero} alt="asmazabeen13@gmail.com" />
             </div>
         </div>
     )
